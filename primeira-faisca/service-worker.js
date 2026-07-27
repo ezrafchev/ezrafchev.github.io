c3ec8db28cb43f51df000ee040f9248a4de49924
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2026-07-27 14:15:38 -0300

    Adiciona funcionamento offline do Primeira Faísca

---
+++ primeira-faisca/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='primeira-faisca-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html')))));

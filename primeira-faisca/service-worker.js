ad7d68385e517e1fdd7469c1c09d8a4349494b4c
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2026-07-27 16:43:39 -0300

    Atualiza cache para a correção v7.2

--- primeira-faisca/service-worker.js
+++ primeira-faisca/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='primeira-faisca-v7-1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./style-v7.css','./style-v7-core.css','./style-v7-ui.css','./app-v7.js','./data-cards-v3.js','./data-tarot-v3.js','./core-v7-1.js','./core-v7-2.js','./core-v7-3.js','./core-v7-4.js'];
+const CACHE='primeira-faisca-v7-2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./style-v7.css','./style-v7-core.css','./style-v7-ui.css','./app-v7.js','./data-cards-v3.js','./data-tarot-v3.js','./core-v7-1.js','./core-v7-2.js','./core-v7-3.js','./core-v7-4.js','./hotfix-v7-2.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin!==location.origin)return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));});

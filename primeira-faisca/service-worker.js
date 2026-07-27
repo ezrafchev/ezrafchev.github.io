5a4f584da12322eb434b61285925d5a16501fb74
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2026-07-27 14:58:40 -0300

    Atualiza cache offline para Primeira Faísca v3

--- primeira-faisca/service-worker.js
+++ primeira-faisca/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='primeira-faisca-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
-self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html')))));
+const CACHE='primeira-faisca-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./style-v3.css','./app-v3.js','./data-cards-v3.js','./data-tarot-v3.js','./app-core-v3.js'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html')))));

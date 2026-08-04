34a10b41c818bea505f90f710609897b3402c124
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2026-08-04 08:34:02 -0300

    Adiciona callback de confirmação de email

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { createClient } from '@/src/lib/supabase-server';
+
+export async function GET(request: NextRequest) {
+  const url = new URL(request.url);
+  const code = url.searchParams.get('code');
+  if (code) {
+    const supabase = createClient();
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+  return NextResponse.redirect(new URL('/conta', request.url));
+}
